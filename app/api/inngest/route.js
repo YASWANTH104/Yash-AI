e3b2231a3a675adab36582d3ac4ce14fdc510efb
Yaswanth sai Gonuguntla <gyaswanthsai2004@gmail.com>
2025-09-07 14:42:21 +0530

    Bug Fixes

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,6 +1,6 @@
 import { serve } from "inngest/next";
 import { inngest } from "@/lib/inngest/client";
-import { generateIndustryInsights } from "@/lib/inngest/functions";
+import { generateIndustryInsights } from "@/lib/inngest/function";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
